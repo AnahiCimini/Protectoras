a4af10650e06a52f35a528ffcaca1f55daa133a2
Noelia <anahicimini@gmail.com>
2025-01-31 01:23:28 +0100

    Añadiendo des-selección automática de un botón al seleccionar otro, en el listado protectoras - CCAA

--- src/views/listadoProtectorasView.php
+++ src/views/listadoProtectorasView.php
@@ -1,5 +1,5 @@
 <div class="container-fluid">
-    <div class="row d-flex flex-wrap">
+    <div class="row d-flex flex-wrap" id="accordionCCAA">
         <?php foreach ($ccaas as $ccaa): ?>
             <div class="col-12 col-md-3 mb-3 rows_ccaa">
                 <!-- Botón de CCAA -->
@@ -9,10 +9,10 @@
             </div>
 
             <!-- Contenedor de provincias colapsable -->
-            <div id="collapse<?php echo $ccaa['id_ccaa']; ?>" class="collapse provincias_collapse" data-bs-parent="#accordionExample">
+            <div id="collapse<?php echo $ccaa['id_ccaa']; ?>" class="collapse provincias_collapse" data-bs-parent="#accordionCCAA">
                 <div class="container-fluid">
-                    <div class="row d-flex">
-                        <?php
+                <div class="row d-flex" id="accordionProvincias<?php echo $ccaa['id_ccaa']; ?>">
+                    <?php
                         foreach ($provincias as $provincia):
                             if ($provincia['id_ccaa'] == $ccaa['id_ccaa']):
                                 $protectoraModel = new Protectora($conn);
@@ -25,7 +25,7 @@
                                 </button>
                                 
                                 <!-- Contenedor de protectoras colapsables -->
-                                <div id="collapse-provincia-<?php echo $provincia['id_provincia']; ?>" class="collapse">
+                                <div id="collapse-provincia-<?php echo $provincia['id_provincia']; ?>" class="collapse" data-bs-parent="#accordionProvincias<?php echo $ccaa['id_ccaa']; ?>">
                                     <div class="container-fluid">
                                         <div class="row">
                                             <?php if (!empty($protectoras)): ?>
